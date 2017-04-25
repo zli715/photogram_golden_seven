1ff744a69435d73b94a8b32b880d572bfc6d9083
Raghu Betina <raghu@firstdraft.com>
2017-04-25 14:08:32 -0500

    Better hints

--- spec/features/photos_spec.rb
+++ spec/features/photos_spec.rb
@@ -110,54 +110,69 @@ feature "Index page" do
   end
 end
 
-feature "New photo page" do
+feature "New photo form" do
   it "has a functional RCAV", points: 1 do
     visit "/photos/new"
 
     expect(page)
   end
+end
 
+feature "New photo form" do
   it "has a form", points: 1 do
     visit "/photos/new"
 
     expect(page).to have_css("form", count: 1)
   end
+end
 
+feature "New photo form" do
   it "has a label for 'Caption'", points: 1, hint: h("copy_must_match label_for_input") do
     visit "/photos/new"
 
     expect(page).to have_css("label", text: "Caption")
   end
+end
 
+feature "New photo form" do
   it "has a label for 'Image URL'", points: 1, hint: h("copy_must_match label_for_input") do
     visit "/photos/new"
 
     expect(page).to have_css("label", text: "Image URL")
   end
+end
 
+feature "New photo form" do
   it "has two inputs", points: 1, hint: h("label_for_input") do
     visit "/photos/new"
 
     expect(page).to have_css("input", count: 2)
   end
+end
 
-  it "has a button to 'Create Photo'", points: 1, hint: h("copy_must_match") do
+feature "New photo form" do  it "has a button to 'Create Photo'", points: 1, hint: h("copy_must_match") do
     visit "/photos/new"
 
     expect(page).to have_css("button", text: "Create Photo")
   end
+end
 
-  it "creates a photo when submitted", points: 3 do
+feature "New photo form" do
+  it "creates a photo when submitted", points: 3, hint: h("button_type") do
     initial_number_of_photos = Photo.count
 
     visit "/photos/new"
+
     click_on "Create Photo"
 
     final_number_of_photos = Photo.count
+
     expect(final_number_of_photos).to eq(initial_number_of_photos + 1)
   end
+end
 
-  it "saves the caption when submitted", points: 2, hint: "Be sure your label is 'Caption' and is tied to the correct input through its `for=\"\"` attribute" do
+feature "New photo form" do
+  it "saves the caption when submitted", points: 2, hint: h("label_for_input") do
     test_caption = "Photogram test caption, added at time #{Time.now}."
 
     visit "/photos/new"
@@ -167,8 +182,10 @@ feature "New photo page" do
     last_photo = Photo.order(created_at: :asc).last
     expect(last_photo.caption).to eq(test_caption)
   end
+end
 
-  it "saves the image URL when submitted", points: 2, hint: "Be sure your label is 'Image URL' and is tied to the correct input through its `for=\"\"` attribute" do
+feature "New photo form" do
+  it "saves the image URL when submitted", points: 2, hint: h("label_for_input") do
     test_source = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Pluto-01_Stern_03_Pluto_Color_TXT.jpg/240px-Pluto-01_Stern_03_Pluto_Color_TXT.jpg"
 
     visit "/photos/new"
@@ -180,7 +197,7 @@ feature "New photo page" do
   end
 end
 
-feature "Delete photo" do
+feature "New photo form" do
   it "removes a row from the table", points: 5 do
     photo = create(:photo)
 
@@ -188,17 +205,19 @@ feature "Delete photo" do
 
     expect(Photo.exists?(photo.id)).to be false
   end
+end
 
+feature "New photo form" do
   it "redirects user to the index page", points: 3 do
     photo = create(:photo)
 
     visit "/delete_photo/#{photo.id}"
 
-    expect(page).to have_current_path("/photos") # should not depend on path
+    expect(page).to have_current_path("/photos")
   end
 end
 
-feature "Edit photo page" do
+feature "Edit photo form" do
   it "has a functional RCAV", points: 1 do
     photo = create(:photo)
 
@@ -206,7 +225,9 @@ feature "Edit photo page" do
 
     expect(page)
   end
+end
 
+feature "Edit photo form" do
   it "has a form", points: 1 do
     photo = create(:photo)
 
@@ -214,56 +235,70 @@ feature "Edit photo page" do
 
     expect(page).to have_css("form", count: 1)
   end
+end
 
-  it "has two inputs", points: 1 do
-    photo = create(:photo)
-
-    visit "/photos/#{photo.id}/edit"
-
-    expect(page).to have_css("input", count: 2)
-  end
-
-  it "has a label for 'Caption'", points: 1 do
+feature "Edit photo form" do
+  it "has a label for 'Caption'", points: 1, hint: h("copy_must_match label_for_input") do
     photo = create(:photo)
 
     visit "/photos/#{photo.id}/edit"
 
     expect(page).to have_css("label", text: "Caption")
   end
+end
 
-  it "has a label for 'Image URL'", points: 1 do
+feature "Edit photo form" do
+  it "has a label for 'Image URL'", points: 1, hint: h("copy_must_match label_for_input") do
     photo = create(:photo)
 
     visit "/photos/#{photo.id}/edit"
 
     expect(page).to have_css("label", text: "Image URL")
   end
+end
 
-  it "has a button to 'Update Photo'", points: 1 do
+feature "Edit photo form" do
+  it "has two inputs", points: 1, hint: h("label_for_input") do
+    photo = create(:photo)
+
+    visit "/photos/#{photo.id}/edit"
+
+    expect(page).to have_css("input", count: 2)
+  end
+end
+
+feature "Edit photo form" do
+  it "has a button to 'Update Photo'", points: 1, hint: h("label_for_input") do
     photo = create(:photo)
 
     visit "/photos/#{photo.id}/edit"
 
     expect(page).to have_css("button", text: "Update Photo")
   end
+end
 
-  it "has caption prepopulated", points: 3 do
+feature "Edit photo form" do
+  it "has caption prepopulated", points: 3, hint: h("value_attribute") do
     photo = create(:photo, caption: "Old caption")
 
     visit "/photos/#{photo.id}/edit"
 
     expect(page).to have_css("input[value='Old caption']")
   end
+end
 
-  it "has image source prepopulated", points: 3 do
+feature "Edit photo form" do
+  it "has image source prepopulated", points: 3, hint: h("value_attribute") do
     photo = create(:photo, source: "http://old.image/source.jpg")
 
     visit "/photos/#{photo.id}/edit"
 
     expect(page).to have_css("input[value='http://old.image/source.jpg']")
   end
+end
 
-  it "updates caption when submitted", points: 5, hint: "Be sure your label is 'Caption' and is tied to the correct input through its `for=\"\"` attribute" do
+feature "Edit photo form" do
+  it "updates caption when submitted", points: 5, hint: h("label_for_input button_type") do
     photo = create(:photo, caption: "Old caption")
     test_caption = "New caption, added at #{Time.now}"
 
@@ -275,8 +310,10 @@ feature "Edit photo page" do
 
     expect(photo_as_revised.caption).to eq(test_caption)
   end
+end
 
-  it "updates image source when submitted", points: 5, hint: "Be sure your label is 'Image URL' and is tied to the correct input through its `for=\"\"` attribute" do
+feature "Edit photo form" do
+  it "updates image source when submitted", points: 5, hint: h("label_for_input button_type") do
     photo = create(:photo, source: "http://old.image/source.jpg")
     test_source = "http://new.image/source_#{Time.now.to_i}.jpg"
 
@@ -295,6 +332,6 @@ feature "Edit photo page" do
     visit "/photos/#{photo.id}/edit"
     click_on "Update Photo"
 
-    expect(page).to have_current_path("/photos/#{photo.id}") # should not depend on path
+    expect(page).to have_current_path("/photos/#{photo.id}")
   end
 end
